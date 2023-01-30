6d5c042537b362b98e4eb63c122c269367159624
like115 <258861342@qq.com>
2023-01-30 14:31:39 +0800

    Initial commit

--- practice/pr06_openpyxl.py
+++ practice/pr06_openpyxl.py
@@ -2,6 +2,7 @@ from openpyxl import load_workbook
 wb = load_workbook(r'F:\books\工资条.xlsx')
 ws = wb.active
 print(ws)
+<<<<<<< HEAD
 for cell in ws["1"]:
     print(cell.value)
 print('sdsdsd')
@@ -10,3 +11,8 @@ print('sdsdsd')
 # 第二次测试 
 # 第三次测试
 777
+=======
+for cell in ws["2"]:
+    print(cell.value)
+
+>>>>>>> 47a4655 (Initial commit)
