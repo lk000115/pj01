8f9c0f9e9dd985c58c7e2cb4910c997ef2357efb
like115 <258861342@qq.com>
2023-02-01 17:19:38 +0800

    Initial commit

--- practice/pr06_openpyxl.py
+++ practice/pr06_openpyxl.py
@@ -2,6 +2,8 @@ from openpyxl import load_workbook
 wb = load_workbook(r'F:\books\工资条.xlsx')
 ws = wb.active
 print(ws)
-for cell in ws["2"]:
-    print(cell.value)
-
+l=[]
+for cell in ws[1]:
+    l.append(cell.value)
+print(l)
+#jjjjj
