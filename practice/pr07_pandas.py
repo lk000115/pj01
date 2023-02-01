165b9b52fcfc8caf1190992930affae7b5cb3406
like115 <258861342@qq.com>
2023-02-01 17:16:32 +0800

    Initial commit

---
+++ practice/pr07_pandas.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# d = {'a':1,'b':2,'c':3,'d':4,'e':5}
+# df = pd.Series(d)
+df = pd.read_excel(r'F:\books\工资条.xlsx')
+print(df.head().columns.values)  # 打印表头
+print(df.values)  # 打印表的值-不含表头行
+print('------------------')
+lt=[]
+lt=df.head().columns.values
+print(lt)
