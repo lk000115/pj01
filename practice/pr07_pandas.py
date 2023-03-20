87959c1a3ce72d39b1b762302aee10270d9f84e2
like115 <258861342@qq.com>
2023-03-20 13:25:27 +0800

    sd

--- practice/pr07_pandas.py
+++ practice/pr07_pandas.py
@@ -1,11 +1,23 @@
 import pandas as pd
 
-# d = {'a':1,'b':2,'c':3,'d':4,'e':5}
-# df = pd.Series(d)
-df = pd.read_excel(r'F:\books\工资条.xlsx')
-print(df.head().columns.values)  # 打印表头
-print(df.values)  # 打印表的值-不含表头行
-print('------------------')
-tmp=[]
-tmp=df.head().columns.values
-print(tmp)
+
+
+df = pd.read_excel(r'D:\py_sty\pj01\res\gz.xlsx',sheet_name=0)
+# print(df.head().columns.values)  # 打印表头
+head = df.head().columns.values
+for item in df.values:
+    st = ''
+    for i in range(len(item)):  # 把表每一行循环拼接为字符串格式
+        st = st + str(head[i]) + ' : ' + str(item[i]) + '\n'
+    user = str(item[1])
+    print(user)
+    print(st)  # 打印表的值-不含表头行
+print(df.values)
+
+
+
+
+# print('------------------')
+# tmp=[]
+# tmp=df.head().columns.values
+# print(tmp)
